47de9aac6efc0c84aac142a8085510843702b469
zouwei <huzougege@gmail.com>
2026-05-06 12:41:09 +0800

    chore: release v0.53.4

---
+++ app/test/flutter_test_config.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  return GoldenToolkit.runWithConfiguration(
+    () async {
+      await loadAppFonts();
+      await testMain();
+    },
+    config: GoldenToolkitConfiguration(
+      skipGoldenAssertion: () => Platform.isLinux,
+    ),
+  );
+}
